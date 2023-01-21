8808844ac14850183373d7f7f8e1b31bfeb8e659
IlyaSushkevich <ilya.sushkevich@gmail.com>
2023-01-21 12:41:30 +0300

    CRUD Персон с использованием Vue

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,13 +1,57 @@
 <template>
-<div>
+    <div>
+        <CreateComponent></CreateComponent>
+        <SinglePostComponent></SinglePostComponent>
+        <!-- <table class="table">
+            <thead>
+                <tr>
+                    <th class="col">#</th>
+                    <th class="col">Name</th>
+                    <th class="col">Age</th>
+                    <th class="col">job</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="person in persons">
+                    <th scope="row">{{ person.id }}</th>
+                    <td>{{ person.name }}</td>
+                    <td>{{ person.age }}</td>
+                    <td>{{ person.job }}</td>
+                </tr>
+            </tbody>
+        </table> -->
 
-</div>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "PostComponent"
+import SinglePostComponent from './SinglePostComponent.vue';
+import CreateComponent from './CreateComponent.vue';
+
+export default {
+    name: "PostComponent",
+
+
+    data() {
+        return {
+
+        }
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+
+
+    components: {
+        SinglePostComponent,
+        CreateComponent,
     }
+};
 </script>
 
 <style scoped>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
-<?php
+    <?php
 
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
 
-/*
+
+
+    /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -13,10 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Auth::routes();
+    Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/posts', 'App\Http\Controllers\PostController')->name('post');
