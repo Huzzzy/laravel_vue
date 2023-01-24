a9fdb94f858c097d03dfbb9da9ccea1fae6561c3
IlyaSushkevich <ilya.sushkevich@gmail.com>
2023-01-24 12:58:59 +0300

    Удаление ненужных компонентов

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,18 +1,9 @@
 //import { createRouter, createWebHistory } from "vue-router";
 import { createRouter, createWebHistory } from "vue-router/dist/vue-router";
 
-import PostComponent from "./components/PostComponent.vue";
-import TagComponent from "./components/TagComponent.vue";
 
 const routes = [
-    {
-        path: '/posts',
-        component: PostComponent
-    },
-    {
-        path: '/tags',
-        component: TagComponent
-    },
+
 ]
 
 const router = createRouter({
